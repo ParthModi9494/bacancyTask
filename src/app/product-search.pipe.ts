5e7a6636579d01b79bb1707c8115bb8c4d01ad40
Parth <Parthmodi9494@gmail.com>
2021-02-11 12:59:40 +0530

    product search pipe added

---
+++ src/app/product-search.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from './product';
+
+@Pipe({
+  name: 'productSearch'
+})
+export class ProductSearchPipe implements PipeTransform {
+
+  transform(products: { docId: string, data: Product }[], ...args: string[]) {
+    const searchQuery = args[0].toLowerCase();
+    if (!products?.length || !args.length) {
+      return null;
+    }
+    if (args[0] === "") {
+      return products;
+    }
+    return products.filter((product) => {
+      return (
+        product.data.id.toString().toLowerCase().includes(searchQuery) ||
+        product.data.title.toLowerCase().includes(searchQuery) ||
+        product.data.price.toString().toLowerCase().includes(searchQuery) ||
+        product.data.stock.toString().toLowerCase().includes(searchQuery)
+      );
+    })
+  }
+
+}
