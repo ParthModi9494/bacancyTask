348ed9c10702eef3c530f7ee9e7ae3cb7a8ae51c
Parth <Parthmodi9494@gmail.com>
2021-02-11 22:41:21 +0530

    alert service added

---
+++ src/app/alert.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertOptions } from 'sweetalert2';
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  showAlert(options: SweetAlertOptions) {
+    return Swal.fire(options);
+  }
+}
