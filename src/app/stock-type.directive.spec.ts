bf1be5492e783cb7be6491c39ee8b9f46b0d4af6
Parth <Parthmodi9494@gmail.com>
2021-02-11 22:42:28 +0530

    stock-type directive added

---
+++ src/app/stock-type.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StockTypeDirective } from './stock-type.directive';
+
+describe('StockTypeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StockTypeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/stock-type.directive.ts
@@ -0,0 +1,34 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appStockType]'
+})
+export class StockTypeDirective {
+  @Input("stockValue") stockValue: string = "";
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2) { }
+
+  ngOnChanges() {
+    if (!this.stockValue) {
+      this.setLabelData("", "");
+      return;
+    }
+    const stock = Number(this.stockValue);
+    if (stock <= 10) {
+      this.setLabelData("C", "bg-danger");
+    } else if (stock > 10 && stock <= 20) {
+      this.setLabelData("B", "bg-warning");
+    } else if (stock > 20) {
+      this.setLabelData("A", "bg-success");
+    } else {
+      this.setLabelData("", "");
+    }
+  }
+
+  setLabelData(label: string, className: string) {
+    this.renderer.setProperty(this.elementRef.nativeElement, "classList", ["label"]);
+    this.renderer.setProperty(this.elementRef.nativeElement, "innerText", label);
+    this.renderer.addClass(this.elementRef.nativeElement, className);
+  }
+}
