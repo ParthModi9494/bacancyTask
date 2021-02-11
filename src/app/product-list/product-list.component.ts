506f22c2c0e5ad74076585c4111762224db43732
Parth <Parthmodi9494@gmail.com>
2021-02-11 22:41:56 +0530

    product-list component fine tuned

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SweetAlertOptions } from 'sweetalert2';
+import { AlertService } from '../alert.service';
+import { AuthService } from '../auth.service';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,11 +14,159 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  favourites: number = 0;
-  cartItems: number = 0;
-  constructor() { }
-
-  ngOnInit(): void {
+  totalFavourites: number = 0;
+  totalCartItems: number = 0;
+  searchQuery: string = "";
+  isShowingFavourite: boolean = false;
+  isShowingCart: boolean = false;
+  page: number = 2;
+  pageSize = 10;
+  products: { docId: string, data: Product }[] = [];
+  addOREditProductForm: FormGroup;
+  constructor(
+    private productsService: ProductsService,
+    private authService: AuthService,
+    private modalService: NgbModal,
+    private alertService: AlertService,
+    private formBuilder: FormBuilder) {
+    this.addOREditProductForm = this.formBuilder.group({
+      "title": ["", Validators.required],
+      "price": ["", Validators.required],
+      "stock": ["", Validators.required]
+    })
   }
 
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsService.fetchProducts().subscribe((docData) => {
+      this.products = docData;
+      const favourites = this.products.filter((product) => {
+        return product.data.isFavourite;
+      });
+      this.totalFavourites = favourites ? favourites.length : 0;
+
+      const cartItems = this.products.filter((product) => {
+        return product.data.isAddedIntoCart;
+      });
+      this.totalCartItems = favourites ? cartItems.length : 0;
+    }, (err: HttpErrorResponse) => {
+      this.products = [];
+    });
+  }
+
+  deleteProduct(docId: string) {
+    const options: SweetAlertOptions = {
+      title: "Are you sure?",
+      text: "You want to delete this product?",
+      icon: "warning",
+      showDenyButton: true,
+      showConfirmButton: true
+    };
+    this.alertService.showAlert(options).then((selection) => {
+      if (selection.isConfirmed) {
+        this.productsService.deleteProduct(docId).then((data) => {
+          this.alertService.showAlert({ title: "Success", icon: "success", timer: 2000 });
+        }).catch(() => {
+          this.alertService.showAlert({ title: "Error!", icon: "error", timer: 2000 });
+        });
+      }
+    });
+  }
+
+  addProduct() {
+    const newProduct: Product = {
+      id: this.products.length + 1,
+      isAddedIntoCart: false,
+      isFavourite: false,
+      price: this.addOREditProductForm.value.price,
+      stock: this.addOREditProductForm.value.stock,
+      title: this.addOREditProductForm.value.title
+    };
+    if (newProduct.title.length > 0 && newProduct.price && newProduct.stock) {
+      this.productsService.addProduct(newProduct).then(() => {
+        this.alertService.showAlert({ title: "Success", text: "Product successfully added!", icon: "success", timer: 2000 });
+      });
+    } else {
+      this.alertService.showAlert({ title: "Please provide valid details!", icon: "error", timer: 2000 });
+    }
+  }
+
+  editProduct(docId: string) {
+    const productToUpdate: Product = {
+      price: this.addOREditProductForm.value.price,
+      stock: this.addOREditProductForm.value.stock,
+      title: this.addOREditProductForm.value.title
+    };
+    if (productToUpdate.title.length > 0 && productToUpdate.price && productToUpdate.stock) {
+      this.productsService.editProduct(docId, productToUpdate).then(() => {
+        this.alertService.showAlert({ title: "Success", text: "Product data successfully updated!", icon: "success", timer: 2000 });
+      });
+    } else {
+      this.alertService.showAlert({ title: "Please provide valid details!", icon: "error", timer: 2000 });
+    }
+  }
+
+  addOrRemoveCart(docId: string, productData: Product) {
+    this.productsService.addOrRemoveCart(docId, !productData.isAddedIntoCart).then((data) => {
+      if (productData.isAddedIntoCart) {
+        this.alertService.showAlert({ title: "Success", text: "Product successfully removed from Cart!", icon: "success", timer: 2000 });
+      } else {
+        this.alertService.showAlert({ title: "Success", text: "Product successfully added into Cart!", icon: "success", timer: 2000 });
+      }
+    }).catch((err) => {
+      this.alertService.showAlert({ title: "Error!", icon: "error", timer: 2000 });
+    });
+  }
+
+  addOrRemoveFavourite(docId: string, productData: Product) {
+    this.productsService.addOrRemoveFavourite(docId, !productData.isFavourite).then((data) => {
+      if (productData.isFavourite) {
+        this.alertService.showAlert({ title: "Success", text: "Product removed from Favourites!", icon: "success", timer: 2000 });
+      } else {
+        this.alertService.showAlert({ title: "Success", text: "Product marked as Favourite!", icon: "success", timer: 2000 });
+      }
+    }).catch((err) => {
+      this.alertService.showAlert({ title: "Error!", icon: "error", timer: 2000 });
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  addOrEditProduct(content: TemplateRef<HTMLElement>, product?: { docId: string, data: Product }) {
+    this.addOREditProductForm.patchValue({
+      title: product ? product.data.title : "",
+      price: product ? product.data.price : "",
+      stock: product ? product.data.stock : ""
+    });
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then(() => {
+      product ? this.editProduct(product.docId) : this.addProduct();
+    }, (reason) => { });
+  }
+
+  getFavourites() {
+    if (this.isShowingFavourite) {
+      this.getProducts();
+    } else {
+      this.products = this.products.filter((product) => {
+        return product.data.isFavourite;
+      })
+    }
+    this.isShowingFavourite = !this.isShowingFavourite;
+  }
+
+  getCartItems() {
+    if (this.isShowingCart) {
+      this.getProducts();
+    } else {
+      this.products = this.products.filter((product) => {
+        return product.data.isAddedIntoCart;
+      })
+    }
+    this.isShowingCart = !this.isShowingCart;
+  }
 }
